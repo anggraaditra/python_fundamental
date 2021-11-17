61cfacb672deac42945f9314b9d94e31eadf4b5e
Anggra Aditra <aditra.anggra@gmail.com>
2021-11-17 21:01:09 +0700

    sequential syntax

---
+++ python_basic_syntax_sequential.py
@@ -0,0 +1,9 @@
+print('hello world!')
+
+jumlah_susu = 4
+
+# sequential sintax - syntaksis percabangan
+if jumlah_susu > 5:
+    print('budi beli susu di warung')
+else:
+    print('budi tidak jadi beli susu di warung')
